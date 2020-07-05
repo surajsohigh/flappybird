9e19f8ee03ef61e2248e8c682609283a6735d954
Suraj Gupta <skrg141@gmail.com>
2020-07-05 17:05:25 +0530

    global variable updated

--- flappybird.py
+++ flappybird.py
@@ -1,1 +1,16 @@
-print ("WELCOME TO FLAPPYBIRD GAME")
+import random   #Forgenereting random no
+import sys      #we will use sys.exit to exit the program
+import pygame   #
+from pygame.locals import *      #Basis pygame imports
+
+FPS=32
+SCREENWIDTH = 289
+SCREENHEIGHT = 511
+SCREEN = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
+GROUNDY = SCREENHEIGHT * 0.8
+GAME_SPRITES = {}
+GAME_SOUND = {}
+PLAYER = 'gallery/sprites/bird.png'
+BACKGROUND = 'gallery/sprites/background.png'
+PIPE = 'gallery/sprites/pipe.png'
+
