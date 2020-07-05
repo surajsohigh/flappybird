cae322903c48414a4525c3fa05ccd2a4023843c2
Suraj Gupta <skrg141@gmail.com>
2020-07-05 17:14:32 +0530

    init main

--- flappybird.py
+++ flappybird.py
@@ -14,3 +14,22 @@ PLAYER = 'gallery/sprites/bird.png'
 BACKGROUND = 'gallery/sprites/background.png'
 PIPE = 'gallery/sprites/pipe.png'
 
+
+
+if __name__ == '__main__':   #main function to start
+    pygame.init()            #initate all pygame module
+    FPSCLOCK = pygame.time.Clock()
+    pygame.display.set_caption("Flappy Bird ")
+    GAME_SPRITES['numbers'] = (
+        pygame.image.load('gallery/sprites/0.png').convert_alpha(),
+        pygame.image.load('gallery/sprites/1.png').convert_alpha(),
+        pygame.image.load('gallery/sprites/2.png').convert_alpha(),
+        pygame.image.load('gallery/sprites/3.png').convert_alpha(),
+        pygame.image.load('gallery/sprites/4.png').convert_alpha(),
+        pygame.image.load('gallery/sprites/5.png').convert_alpha(),
+        pygame.image.load('gallery/sprites/6.png').convert_alpha(),
+        pygame.image.load('gallery/sprites/7.png').convert_alpha(),
+        pygame.image.load('gallery/sprites/8.png').convert_alpha(),
+        pygame.image.load('gallery/sprites/9.png').convert_alpha(),
+    )
+
