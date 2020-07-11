c0c3956cbcc0425fafed4b8617957b446ad48b81
Suraj Gupta <skrg141@gmail.com>
2020-07-11 22:54:34 +0530

    mainscreen

--- flappybird.py
+++ flappybird.py
@@ -15,6 +15,37 @@ BACKGROUND = 'gallery/sprites/background.png'
 PIPE = 'gallery/sprites/pipe.png'
 
 
+def welcomeScreen():
+    """
+    Shows welcome images on the screen
+    """
+
+    playerx = int(SCREENWIDTH/5)
+    playery = int((SCREENHEIGHT - GAME_SPRITES['player'].get_height())/2)
+    #messagex = int((SCREENWIDTH - GAME_SPRITES['message'].get_width())/2)
+    #messagey = int(SCREENHEIGHT*0.13)
+    basex = 0
+    while True:
+        for event in pygame.event.get():
+            # if user clicks on cross button, close the game
+            if event.type == QUIT or (event.type==KEYDOWN and event.key == K_ESCAPE):
+                pygame.quit()
+                sys.exit()
+
+            # If the user presses space or up key, start the game for them
+            elif event.type==KEYDOWN and (event.key==K_SPACE or event.key == K_UP):
+                return
+            else:
+                SCREEN.blit(GAME_SPRITES['background'], (0, 0))
+                SCREEN.blit(GAME_SPRITES['player'], (playerx, playery))
+                #SCREEN.blit(GAME_SPRITES['message'], (messagex,messagey ))
+                SCREEN.blit(GAME_SPRITES['base'], (basex, GROUNDY))
+                pygame.display.update()
+                FPSCLOCK.tick(FPS)
+
+
+
+
 
 if __name__ == '__main__':   #main function to start
     pygame.init()            #initate all pygame module
