ba1c8049fce663e6cbcde46cfc17b6f577f24223
Suraj Gupta <skrg141@gmail.com>
2020-07-31 17:44:47 +0530

    collid code

--- flappybird.py
+++ flappybird.py
@@ -44,6 +44,26 @@ def welcomeScreen():
                 FPSCLOCK.tick(FPS)
 
 
+def isCollide(playerx, playery, upperPipes, lowerPipes):
+    if playery > GROUNDY - 25 or playery < 0:
+        GAME_SOUNDS['hit'].play()
+        return True
+
+    for pipe in upperPipes:
+        pipeHeight = GAME_SPRITES['pipe'][0].get_height()
+        if (playery < pipeHeight + pipe['y'] and abs(playerx - pipe['x']) < GAME_SPRITES['pipe'][0].get_width()):
+            GAME_SOUNDS['hit'].play()
+            return True
+
+    for pipe in lowerPipes:
+        if (playery + GAME_SPRITES['player'].get_height() > pipe['y']) and abs(playerx - pipe['x']) < \
+                GAME_SPRITES['pipe'][0].get_width():
+            GAME_SOUNDS['hit'].play()
+            return True
+
+    return False
+
+
 def getRandomPipe():
     """
     Generate positions of two pipes(one bottom straight and one top rotated ) for blitting on the screen
